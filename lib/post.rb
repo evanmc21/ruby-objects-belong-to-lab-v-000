bd2a349d6d1a7656fabd0ede8a62f9fc623b737d
Evan Candler <emcandler21@gmail.com>
2018-03-01 21:31:20 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,13 @@
 class Post
-  
+  attr_accessor :author, :title
+
+    def initialize(name)
+      @title = name
+    end
+    def author_name
+      if @author == nil
+        nil
+      else
+        self.author.name
+      end
 end
